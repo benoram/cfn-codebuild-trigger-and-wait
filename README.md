dffe89b4c1481d9d1285eaba280eb3c1b32d45c5
Ben Oram <b@oram.co>
2020-03-21 09:17:54 -0600

    Use the same CICD process as other CFN-* repos

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # cfn-codebuild-tigger-and-wait
 
-A CloudFormation script that uses a custom resource to trigger CodeBuild and wait for success. A failed build will fail the custom resource.
+This CloudFormation custom resource can be used to start the build of a codebuild project and wait for a successful build result.
+
+This is useful when you need a build to execute within CloudFormation before using the results from that build in something else. I use it for
+
+- Pushing CFN script to a bucket so they can be used as nested stacks later in the script.
+- Pushing code to an S3 bucket so when the CloudFormation script completes my static websites are working.
+
+## How to use
+
+See ```./example.yaml``` for an example script that leverages the function. 
+
+## Other notes
+
+This project is C# and can be built with the SDK, or with an IDE like Visual Studio Community
--- src/BuildAndWait.cs
+++ src/BuildAndWait.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -36,60 +37,68 @@ namespace OramCo.CloudFormation.CustomResource.CodeBuild
             response.Status = "FAILED";
             var resourceProperties = request.resourceProperties.ToObject<CustomResourceProperties>();
 
-            switch (request.requestType.ToLower())
+            try
             {
-                case "create":
-                case "update":
-                    var buildRequest = new StartBuildRequest();
-                    buildRequest.ProjectName = resourceProperties.CodeBuildProjectName;
+                switch (request.requestType.ToLower())
+                {
+                    case "create":
+                    case "update":
+                        var buildRequest = new StartBuildRequest();
+                        buildRequest.ProjectName = resourceProperties.CodeBuildProjectName;
 
-                    var buildClient = new AmazonCodeBuildClient();
+                        var buildClient = new AmazonCodeBuildClient();
 
-                    LambdaLogger.Log($"Starting build: {buildRequest.ProjectName}");
-                    var buildResponse = await buildClient.StartBuildAsync(buildRequest);
-                    
-                    if (buildResponse.HttpStatusCode == System.Net.HttpStatusCode.OK)
-                    {
-                        var statusRequest = new BatchGetBuildsRequest();
-                        statusRequest.Ids.Add(buildResponse.Build.Id);
-
-                        while (true)
+                        LambdaLogger.Log($"Starting build: {buildRequest.ProjectName}");
+                        var buildResponse = await buildClient.StartBuildAsync(buildRequest);
+                        
+                        if (buildResponse.HttpStatusCode == System.Net.HttpStatusCode.OK)
                         {
-                            System.Threading.Thread.Sleep(5000); // Wait 5 seconds between build status checks
-                            
-                            var statusResponse = await buildClient.BatchGetBuildsAsync(statusRequest);                            
+                            var statusRequest = new BatchGetBuildsRequest();
+                            statusRequest.Ids.Add(buildResponse.Build.Id);
 
-                            Debug.Assert(statusResponse.Builds.Count == 1);
+                            while (true)
+                            {
+                                System.Threading.Thread.Sleep(5000); // Wait 5 seconds between build status checks
+                                
+                                var statusResponse = await buildClient.BatchGetBuildsAsync(statusRequest);                            
 
-                            LambdaLogger.Log($"Build Id: {statusResponse.Builds[0].Id}; Build Status: {statusResponse.Builds[0].BuildStatus}");
-                            var buildStatus = statusResponse.Builds[0].BuildStatus;
-                            if (buildStatus == StatusType.SUCCEEDED)
-                            {
-                                response.Status = "SUCCESS";
-                                break;
+                                Debug.Assert(statusResponse.Builds.Count == 1);
+
+                                LambdaLogger.Log($"Build Id: {statusResponse.Builds[0].Id}; Build Status: {statusResponse.Builds[0].BuildStatus}");
+                                var buildStatus = statusResponse.Builds[0].BuildStatus;
+                                if (buildStatus == StatusType.SUCCEEDED)
+                                {
+                                    response.Status = "SUCCESS";
+                                    break;
+                                }
+                                else if (buildStatus == StatusType.IN_PROGRESS)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    response.Status = "FAILED";
+                                    break;
+                                }
+                                
                             }
-                            else if (buildStatus == StatusType.IN_PROGRESS)
-                            {
-                                continue;
-                            }
-                            else
-                            {
-                                response.Status = "FAILED";
-                                break;
-                            }
-                            
                         }
-                    }
-                    else
-                    {
-                        LambdaLogger.Log($"StartBuild Failed: {buildResponse.HttpStatusCode}: {buildResponse}");
+                        else
+                        {
+                            LambdaLogger.Log($"StartBuild Failed: {buildResponse.HttpStatusCode}: {buildResponse}");
 
-                    }                    
-                    break;
-                case "delete":
-                    LambdaLogger.Log("Received DELETE request");
-                    response.Status="SUCCESS";
-                    break;
+                        }                    
+                        break;
+                    case "delete":
+                        LambdaLogger.Log("Received DELETE request");
+                        response.Status="SUCCESS";
+                        break;
+                }
+            } 
+            catch (Exception e)
+            {
+                // Swallow exception, we will just pass it back as-is to CloudFormation
+                response.Reason = e.Message;                
             }
 
             context.Logger.LogLine($"Calling CloudFormation callback; status: {response.Status}");
